6493bb71cd712f862246702b0f794b8a53206dad
67110028-alt <67110028@dpu.ac.th>
2025-09-10 15:24:12 +0700

    edit test002

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import myfunc
-a=5
-b=15
-result =myfunc.add(a,b)
-print(f"ผลลัพธ์={result}")
+from myfunc import add
+
+def main():
+    try:
+        a=float(intput("a="))
+        b=float(intput("b="))
+    result =add(a,b)
+    print(f"ผลลัพธ์={result}")
+    except ValueError:
+if __name__=="__main__":
+    main()
